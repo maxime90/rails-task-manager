9aeb89885533e013760dd5cb36cef52221fc1a56
Maxime Jeantet <maximejeantet12@gmail.com>
2018-11-20 15:47:41 +0100

    To DO

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # read all
+  get '/tasks', to: 'tasks#index'
+  # create
+  # Step 1: Afficher la form a l'utilisateur avec methode get.
+  get 'tasks/new', to: 'tasks#new'
+  # #reall one
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  # create
+  # Step 2: Recuperer les infos avec la methode Post.
+  post 'tasks', to: 'tasks#create'
+  # # update
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  # post
+  patch 'tasks/:id', to: 'tasks#update'
+  # delete
+  # post
 end
